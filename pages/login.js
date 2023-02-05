f84f0bcc3d053d4eb6ee006fb8c6f9247563938c
Mochammad Bagas <contact.mochammadbagas@gmail.com>
2023-02-05 14:54:39 +0700

    Add Login Form

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import Layout from '@/layout/layout';
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Login() {
   return (
@@ -7,7 +8,38 @@ export default function Login() {
       <Head>
         <title>Login</title>
       </Head>
-      <h1>Login</h1>
+      <section className='w-3/4 mx-auto flex flex-col gap-10'>
+        <div className='title'>
+          <h1 className='text-gray-800 text-4xl font-bold py-4'>Explore</h1>
+          <p className='w-3/4 mx-auto text-gray-400'>
+            Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+          </p>
+        </div>
+        <form className='flex flex-col gap-5'>
+          <div className='input-group'>
+            <input type='email' name='email' placeholder='Email' />
+          </div>
+          <div className='input-group'>
+            <input type='password' name='password' placeholder='Password' />
+          </div>
+
+          <div className='input-button'>
+            <button type='submit'>Login</button>
+          </div>
+          <div className='input-button'>
+            <button type='submit'>Sign in with Google</button>
+          </div>
+          <div className='input-button'>
+            <button type='submit'>Sign in with Github</button>
+          </div>
+        </form>
+        <p className='text-center text-gray-400'>
+          {`don't have an account yet?`}
+          <Link href={'/register'} legacyBehavior>
+            <a className='text-blue-700'> Sign Up</a>
+          </Link>
+        </p>
+      </section>
     </Layout>
   );
 }
